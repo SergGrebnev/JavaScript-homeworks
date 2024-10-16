2e21c5e727796800d5c62bad460d6f50b77fc7ec
SergGrebnev <serggrebnev79@gmail.com>
2024-10-17 01:17:06 +0700

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
+//сравнение значений двух массивов
 function compareArrays(arr1, arr2) {
-  
+    if(arr1.length === arr2.length) {
+        return arr1.every((number, index) => number === arr2[index]);
+    } else {
+        return false;
+    }
+    
 }
 
+//возвращает среднее значение возраста пользователей одного пола
 function getUsersNamesInAgeRange(users, gender) {
+    return users
+        .filter(user => user.gender === gender)
+        .map(user => user.age)
+        .reduce((acc, item, index, arr) => acc + item / arr.length, 0);
   
 }
+   
