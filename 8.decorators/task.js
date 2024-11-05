5e63103bdd6cf7d47afd78f284a8d5b12c61d657
SergGrebnev <serggrebnev79@gmail.com>
2024-11-05 22:09:22 +0700

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,47 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+function wrapper(...args) {
+    const hash = md5(args); // получаем правильный хеш c помощью функции md5
+    let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+    if (objectInCache) { // если элемент найден
+        console.log("Из кеша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+        return "Из кеша: " + objectInCache.value;
+    }
+
+    let result = func(...args); // в кеше результата нет — придётся считать
+    cache.push({hash: md5(args), value: result}) ; // добавляем элемент с правильной структурой
+    if (cache.length > 5) { 
+        cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+}
+return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId = null; // начальное значение таймаута
+    function wrapper(...args){
+        wrapper.allCount ++; // количество вызовов декоратора
+
+        if(timeoutId){ //если таймаут существует, то удаляем его
+            clearTimeout(timeoutId);
+        }
+        if(!timeoutId){ // Первый вызов происходит моментально
+            func(...args);
+            wrapper.count ++; // количество вызовов декорированной функции
+        }
+        
+        timeoutId = setTimeout(() => { //последующие вызовы через интервал
+            func(...args);
+            wrapper.count ++; // количество вызовов декорированной функции
+        }, delay);
+    }
+    wrapper.count = 0; // количество вызовов декорированной функции
+    wrapper.allCount = 0; // количество вызовов декоратора
+    return wrapper;
   
 }
